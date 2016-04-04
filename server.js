dba28314d5d3e9a7adcd6208f86aa4ad4b16c799
Joel Aguero <joelmaguero@gmail.com>
2016-04-04 11:48:13 -0700

    Add comments for server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+// Import necessary packages.
 var express = require('express');
 var app = express();
 
+// Let Heroku set the application port or set it ourselves.
 var port = process.env.PORT || 3000;
 
+// Set the view engine as EmbeddedJS (EJS).
 app.set('view engine', 'ejs');
 
+// Make express look in the public directory for assets.
 app.use(express.static(__dirname + '/public'));
 
+// Set the home page route.
 app.get('/', function(req, res) {
   res.render('index');
 });
